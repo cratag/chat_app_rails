ad32194039831d240c8b11365a95d92220140eb1
Manuel Faner <devcratag@gmail.com>
2023-03-15 12:07:18 -0300

    Chat using Websocket protocol (ActionCable). Chatroom channel. Messages as partials. Better input box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,23 @@
 
 //= require semantic-ui
 
+scroll_bottom = function () {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+submit_message = function () {
+  $('#message_body').on('keydown', function (e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    }
+  })
+}
 
 $(document).on('turbolinks:load', function () {
-  // Semantic UI
-
+  /* Semantic UI */
   // Enable dropdown by targetting.
   $('.ui.dropdown').dropdown();
 
@@ -30,4 +43,11 @@ $(document).on('turbolinks:load', function () {
   $('.message .close').on('click', function () {
     $(this).closest('.message').transition('fade');
   });
+
+
+
+  // Scroll to the bottom of messages box.
+  scroll_bottom();
+  // Listener for message sending.
+  submit_message();
 })
